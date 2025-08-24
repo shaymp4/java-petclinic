99c942103b793429789601d221c652a11725bc1e
treeshay <shayleighton24@gmail.com>
2025-08-24 14:51:08 +0100

    Added PetClinic.java
    Create PetClinic.java with main() and a simple menu loop (no features yet, just print menu).

---
+++ src/PetClinic.java
@@ -0,0 +1,18 @@
+public class PetClinic {
+    public static void main(String[] arg){
+        /**
+         * Menu display
+         */
+        boolean menu = true;
+        while(menu){
+            System.out.println("1. Add pet");
+            System.out.println("2. Show all pets");
+            System.out.println("3. Edit pet");
+            System.out.println("4. Delete pet");
+            System.out.println("5. Report");
+            System.out.println("6. Exit");
+            break;
+        }
+
+    }
+}
