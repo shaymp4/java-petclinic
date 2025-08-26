d5220c0c904c80d222c80a6fe0908bb282b13e70
treeshay <shayleighton24@gmail.com>
2025-08-26 20:11:17 +0100

    Started the addPet() function
    A user is now able to enter type and name of the pet they wish to add.
    
    Still to add:
    -age
    -color
    -weight
    -breed

--- src/PetClinic.java
+++ src/PetClinic.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class PetClinic {
+    private static Scanner input = new Scanner(System.in); // Create an instance of scanner
+
     public static void main(String[] arg){
         /**
          * Menu display
@@ -12,6 +16,31 @@ public class PetClinic {
             System.out.println("5. Report");
             System.out.println("6. Exit");
             break;
+        }
+    }
+
+    private static void addPet(){
+        try {
+            //Pet Type
+            System.out.println("Enter pet type (Cat / Dog): ");
+            String petType = input.nextLine();
+            if(!petType.equalsIgnoreCase("Cat") &&
+            !petType.equalsIgnoreCase("Dog")){
+                System.out.println("Invalid pet type. Please enter a valid pet type.");
+                addPet();
+                return;
+            }
+            //Pet Name
+            System.out.println("Enter name: ");
+            String petName = input.nextLine();
+            if(petName.isEmpty()){
+                System.out.println("Name can't be empty. Please enter a valid name.");
+                addPet();
+                return;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error adding pet.");
         }
 
     }
